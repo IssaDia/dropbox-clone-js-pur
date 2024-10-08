dd3c5fcec58387bbe6b39a6455592374cc97bddd
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-10-08 20:16:02 +0200

    handle register method

--- src/presentation/templates/login/index.ts
+++ src/presentation/templates/login/index.ts
@@ -4,8 +4,33 @@ import Handlebars from "handlebars";
 const loginTemplate: Handlebars.TemplateDelegate = require("./index.hbs");
 
 const login = () => {
-  const html = loginTemplate({});
+  const data = {
+    title: "Se connecter",
+
+    googleButtonId: "google-button-id",
+    appleButtonId: "apple-button-id",
+    handleRegister: handleRegister,
+  };
+  document.title = data.title;
+
+  const html = loginTemplate(data);
+  initializeEvents();
+
   return html;
 };
+
+const handleRegister = (event: Event): void => {
+  const mouseEvent = event as MouseEvent;
+  console.log(mouseEvent.target);
+};
+
+function initializeEvents(): void {
+  document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".register-button");
+    buttons.forEach((button) => {
+      button.addEventListener("click", handleRegister);
+    });
+  });
+}
 
 export default login;
