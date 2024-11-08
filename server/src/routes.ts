69ce44b4bbf67382f3f861e8c97965f64f72e586
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-11-08 21:48:45 +0100

    work on email implementation via provider

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,11 +4,16 @@ import {
   googleAuth,
   googleAuthCallback,
 } from "./controllers/GoogleAuthController";
+import {
+mailAuth
+} from "./controllers/MailAuthController";
 
 const router = Router();
 
 router.get("/auth/google", googleAuth);
 router.get("/auth/google/callback", googleAuthCallback);
+router.get('/auth/token', getAuthToken);
+router.get("/auth/mail", mailAuth);
 router.get('/auth/token', getAuthToken);
 
 
