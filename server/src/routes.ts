2d98e6157bcd66bbfbd5aa1f4aa52d92729ec54f
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-11-09 19:00:00 +0100

    put a refresh token for Google auth

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,21 +1,23 @@
 import { Router } from "express";
-import {
-  getAuthToken,
-  googleAuth,
-  googleAuthCallback,
-} from "./controllers/GoogleAuthController";
-import {
-mailAuth
-} from "./controllers/MailAuthController";
+import GoogleAuthController from "./providers/GoogleAuthProvider";
+import  MailAuthController from "./providers/MailAuthProvider";
+import { checkAndRefreshToken } from "./middlewares/checkRefreshToken";
 
 const router = Router();
 
-router.get("/auth/google", googleAuth);
-router.get("/auth/google/callback", googleAuthCallback);
-router.get('/auth/token', getAuthToken);
-router.get("/auth/mail", mailAuth);
-router.get('/auth/token', getAuthToken);
+// Instancier les contrôleurs
+const googleAuthController = new GoogleAuthController();
+const mailAuthController = new MailAuthController();
+
+router.use("/auth/token", checkAndRefreshToken);
 
 
+// Routes Google Auth
+router.get("/auth/google", (req, res) => googleAuthController.googleAuth(req, res));
+router.get("/auth/google/callback", (req, res) => googleAuthController.googleAuthCallback(req, res));
+router.get("/auth/token", (req, res) => googleAuthController.getAuthToken(req, res));
+
+// Route Mail Auth
+// router.get("/auth/mail", (req, res) => mailAuthController.mailAuth(req, res));
 
 export default router;
