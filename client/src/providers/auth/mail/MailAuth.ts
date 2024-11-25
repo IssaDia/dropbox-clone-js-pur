ccdff8387cff4ee7e2112b7671ab6dbbe04ee679
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-11-25 15:49:42 +0100

    fixed google logo

--- client/src/providers/auth/mail/MailAuth.ts
+++ client/src/providers/auth/mail/MailAuth.ts
@@ -2,8 +2,17 @@ import { RegisterInterface } from "../RegisterInterface";
 
 class MailAuth implements RegisterInterface {
  public static async register(): Promise<any> {
-    console.log("mail");
-    
+    try {
+
+      const response = await fetch("http://localhost:5001/api/auth/google", {
+         credentials: "include",
+       });
+       const data = await response.json();
+
+    } catch  {
+
+    }
+     
  }
 }
 
